6ef1e8db5af5401c6c08857f9f9a498c4ca4f513
vpivkin111111 <97883855+vpivkin111111@users.noreply.github.com>
2022-02-08 09:01:08 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,77 +1,15 @@
 <?php
-using namespace ;
- 
-struct element
-{
-int vik;
-string adres;
-string name;
-};
-void AddStruct(element*z)
-{
-cout <<"\nVvedit name: "; cin>>z->name;
-cin.ignore();
-cout << "\nVvedit adres: "; getline(cin,z->adres);
-cout <<"\nVvedit vik: "; cin>>z->vik;
-}
-struct list;
-{
-element elem;
-list*next;
-};
- 
-typedef list * ptr;
-void prints(element* z)
-{
-cout<<z->adres <<endl;
-cout<<z->name <<endl;
-cout<<z->vik <<endl;
-}
-ptr AddElem(ptr head, element elem)
-{
-ptr x;
-x = (list*)malloc(sizeof(list));
-x->elem=elem;
-x->next = head;
-head = x;
-return head;
-}
- 
-void Print(ptr head)
-{
-for(ptr x = head; x!=NULL; x=x->next)
-prints(&head->elem);
-cout<<endl;
-}
- 
-/*ptr Init(ptr head, int n)
-{
-int elem;
-cout«"Enter number\n";
-for(int i = 0; i<n; i++)
-{
-cin»elem;
-head = AddElem(head, elem);
-}
-return head;
-}*/
- 
-int main()
-{
-ptr head;
-head = NULL; 
-element A;
-cout<<"Vvedit dani!\n";
-AddStruct(&A);
-while(A.name.compare("#")!=0)
-{
-head = AddElem(head,A);
-AddStruct(&A);
-}
-cout<<"\nNew List: ";
-Print(head);
-system("pause");
-return 0;
- 
-}
-?>
+//$arr=(2;5;3;9);
+//masiv(2*5*3*9);
+//echo(2*5*3*9);
+array ("Sophia" => "31", "Jacob" => "41", "William" => "39", "Ramesh" => "40") ;
+echo("Sophia"  "31", "Jacob"  "41", "William"  "39", "Ramesh"  "40");
+
+
+
+
+
+
+
+
+
